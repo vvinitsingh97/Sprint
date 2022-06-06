7b2b8279d9c6049d1389991b601a789b08035b94
praveen7762 <pks19023@gmail.com>
2022-06-06 14:12:34 +0530

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,58 @@
 
 
 #include <iostream>
-#include "../src/date.h"
-#include "../src/customer.h"
-#include "../src/car.h"
-#include "../src/rentals.h"
+#include<fcntl.h>
+#include<pthread.h>
+#include<unistd.h>
+#include "date.h"
+#include "customer.h"
+#include "car.h"
+#include "rentals.h"
 
 using namespace std;
 
 Date todayDate;
+int up = 0;
+pthread_t pthread1;
 
+
+void* userPreferenceThread(void *arg) {
+	char *input = (char *)arg;
+	printf("Started: %s\n", input);
+
+	while(true) {
+
+		//if /tmp/nice file exists
+		if (access("/tmp/nice", F_OK) == 0)
+		{
+			// Config::SetUserPreference(1); //nice menu
+			up = 1;
+		}
+		else
+		{
+			//Config::SetUserPreference(0); //normal menu
+			up = 0;
+		}
+
+		sleep(10);
+	}
+	//pthread_exit(arg);
+	return arg;
+}
+
+void createUserPreferenceThread() {
+	const char *thread_input1 = "User preference thread";
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+	int rc = pthread_create(&pthread1, &attr, userPreferenceThread, (void *)thread_input1);
+	//int rc = pthread_create(&pthread1, NULL, userPreferenceThread, (void *)thread_input1);
+	if(rc != 0) {
+		printf("Error occurred, thread could not be created, errno = %d\n", rc);
+		exit(0);
+	}
+}
 //std::string currentUser;
 /*Function Name  :  printMenu()
  *
@@ -30,21 +73,28 @@ Date todayDate;
  */
 void printMenu() 
 {
-	cout << "=====================================================================================" << endl;
-	cout << "Today\'s Date: " << todayDate << endl;
-	cout << "===================================Welcome to Narmada Car Rental Services ===================================\n\n \
-		\t1. New Car\n \
-		\t2. New Customer\n \
-		\t3. Create Rental\n \
-		\t4. Modify Customer Details\n \
-		\t5. Display Details of a Car\n \
-		\t6. Display Details of all Cars\n \
-		\t7. Display Details of a Customer\n \
-		\t8. Modify Car Detials\n \
-		\t9. Get Rental Data\n \
-		\t10. Print Rental Bill\n \
-		\t11. Exit\n\n";
-	cout << "====================================================================================\n" << endl;
+	if(up == 0)
+	{
+		cout<<"Checking Multithreading"<<endl;
+	}
+	else{
+		cout << "=====================================================================================" << endl;
+		cout << "Today\'s Date: " << todayDate << endl;
+		cout << "===================================Welcome to Narmada Car Rental Services ===================================\n\n \
+			\t1. New Car\n \
+			\t2. New Customer\n \
+			\t3. Create Rental\n \
+			\t4. Modify Customer Details\n \
+			\t5. Display Details of a Car\n \
+			\t6. Display Details of all Cars\n \
+			\t7. Display Details of a Customer\n \
+			\t8. Modify Car Detials\n \
+			\t9. Get Rental Data\n \
+			\t10. Print Rental Bill\n \
+			\t11. Exit\n\n";
+		cout << "====================================================================================\n" << endl;
+		std::string currentUser;
+	}
 }
 
 void waitForKeyPress() 
@@ -68,7 +118,7 @@ int getInput() //This function take the user input as a menu option.
 		cout << "\t\t\t\tChoose: ";
 		cin >> option;
 		cin.ignore();
-	} while(option < 1 || option > 11);
+	} while(option < 1 || option > 12);
 	return option;
 }
 
@@ -97,11 +147,11 @@ void custModify() //this will modify the customer details.
 	cin.ignore();
 	string mn =  "\t\t\t\t1. Modify Name\n\t\t\t\t2. Modify Email\n\t\t\t\t3. Modify Phone Number\n\t\t\t\t4. Modify Residential Address\n\t\t\t\t5. Back\n";
 	int inp = -1;
-	do 
+	do
 	{
 		cout << mn;
 		inp = getInput();
-		if (inp == 1) 
+		if(inp ==1) 
 		{
 			cout << "\t\t\t\tEnter New Name: ";
 			string name;
@@ -151,165 +201,154 @@ void custModify() //this will modify the customer details.
  */ 
 int main() 
 {
+	createUserPreferenceThread();
 	cout << "Enter Today\'s Date: ";
 	todayDate = getDate();
 	int opt = -1;
-	do 
+	while(1)
 	{
 		clear();
+		cout<<"Userpreference"<<up<<endl;
 		printMenu();
 		opt = getInput();
-		if (opt == 1) 
+		switch(opt) 
 		{
-			clear();
-			cout << "=========================== Add New Car ===========================\n" << endl;
-			Car c = getCarDetails();
-			save(c);
-			cout << "===================================================================" << endl;
-			waitForKeyPress();
-			clear();
-		} 
-
-		else if (opt == 2) 
-		{
-			clear();
-			cout << "=========================== New Customer ===========================\n" << endl;
-			Customer cust = registerCustomer();
-			save(cust);
-			cout << "====================================================================" << endl;
-			waitForKeyPress();
-			clear();
-		} 
-
-		else if (opt == 3) 
-		{
-			cout << "============================ New Rental ============================\n" << endl;
-			Rental rent = newRental();
-			save(rent);
-			cout << "====================================================================" << endl;
-			waitForKeyPress();
-			clear();
-		} 
-
-		else if (opt == 4) 
-		{
-			custModify();
-		} 
-
-		else if (opt == 5) 
-		{
-			int c_id;
-			clear();
-			cout << "=========================== Get Car Details ===========================\n" << endl;
-			cout << "\t\t\tEnter Car ID to search: ";
-			cin >> c_id;
-			cin.ignore();
-			clear();
-			Car c = getCar(c_id);
-			cout << "=======================================================================" << endl;
-			cout << c << endl;
-			cout << "\n=======================================================================" << endl;
-			waitForKeyPress();
-			clear();
-		} 
-
-		else if (opt == 6) 
-		{
-			displayAllCars();
-			waitForKeyPress();
-			clear();
-		} 
-
-		else if (opt == 7) 
-		{
-			int cus_id;
-			clear();
-			cout << "=========================== Get Customer Details ===========================\n" << endl;
-			cout << "\t\t\tEnter Customer ID to search: ";
-			cin >> cus_id;
-			cin.ignore();
-			Customer c = getCustomer(cus_id);
-			clear();
-			cout << "============================================================================" << endl;
-			cout << c << endl;
-			cout << "\n==========================================================================" << endl;
-			waitForKeyPress();
-			clear();
-		} 
-
-		else if (opt == 8) 
-		{
-			carModify();
-		} 
-
-		else if (opt == 9) 
-		{
-			int r_id;
-			clear();
-			cout << "=========================== Get Rental Details ===========================\n" << endl;
-			cout << "\t\t\tEnter Rental ID to search: ";
-			cin >> r_id;
-			cin.ignore();
-			Rental r = getRental(r_id);
-			clear();
-			cout << "=================================================================================" << endl;
-			cout << r << endl;
-			cout << "\n=============================================================================" << endl;
-			waitForKeyPress();
-			clear();
-		} 
-
-		else if (opt == 10) 
-		{
-			int r_id;
-			clear();    
-			cout << "=========================== Calculate Rental Bill ===========================\n" << endl;
-			cout << "\t\t\t\tEnter Rental ID: ";
-			cin >> r_id;
-			cin.ignore();
-			Rental r = getRental(r_id);
-			if (!r.chkPaid()) 
-			{
-				double cst = r.calculateBill();
+			case 1:
 				clear();
-				cout << "============================= Bill ===================================\n" << endl;
-				r = getRental(r_id);
+				cout << "=========================== Add New Car ===========================\n" << endl;
+				Car c = getCarDetails();
+				save(c);
+				cout << "===================================================================" << endl;
+				waitForKeyPress();
+				clear(); 
+
+			case 2:
+				clear();
+				cout << "=========================== New Customer ===========================\n" << endl;
+				Customer cust = registerCustomer();
+				save(cust);
+				cout << "====================================================================" << endl;
+				waitForKeyPress();
+				clear();
+
+
+			case 3:
+				cout << "============================ New Rental ============================\n" << endl;
+				Rental rent = newRental();
+				save(rent);
+				cout << "====================================================================" << endl;
+				waitForKeyPress();
+				clear();
+
+			case 4:
+				custModify();
+
+			case 5:
+				int c_id;
+				clear();
+				cout << "=========================== Get Car Details ===========================\n" << endl;
+				cout << "\t\t\tEnter Car ID to search: ";
+				cin >> c_id;
+				cin.ignore();
+				clear();
+				Car c = getCar(c_id);
+				cout << "=======================================================================" << endl;
+				cout << c << endl;
+				cout << "\n=======================================================================" << endl;
+				waitForKeyPress();
+				clear();
+
+			case 6:
+				displayAllCars();
+				waitForKeyPress();
+				clear();
+
+			case 7:
+				int cus_id;
+				clear();
+				cout << "=========================== Get Customer Details ===========================\n" << endl;
+				cout << "\t\t\tEnter Customer ID to search: ";
+				cin >> cus_id;
+				cin.ignore();
+				Customer c = getCustomer(cus_id);
+				clear();
+				cout << "============================================================================" << endl;
+				cout << c << endl;
+				cout << "\n==========================================================================" << endl;
+				waitForKeyPress();
+				clear();
+
+			case 8:
+				carModify();
+
+			case 9:
+				int r_id;
+				clear();
+				cout << "=========================== Get Rental Details ===========================\n" << endl;
+				cout << "\t\t\tEnter Rental ID to search: ";
+				cin >> r_id;
+				cin.ignore();
+				Rental r = getRental(r_id);
+				clear();
+				cout << "=================================================================================" << endl;
 				cout << r << endl;
-				cout << "======================================================================" << endl;
-				cout << "\t\t\tCost to Pay: " << cst << "\t\t" << endl;
-				cout << "\n====================================================================\n" << endl;
-				double cp;
-				do {
-					cout << "\t\t\t\tEnter Cost Paid: ";
-					cin >> cp;
-					cin.ignore();
-					if (cp < cst) {
-						cout << "\t\t\t\tInsufficient Amount" << endl;
-					}
-				} while (cp < cst);
+				cout << "\n=============================================================================" << endl;
+				waitForKeyPress();
+				clear();
 
-				if (cp > cst) 
+
+			case 10:
+				int r_id;
+				clear();    
+				cout << "=========================== Calculate Rental Bill ===========================\n" << endl;
+				cout << "\t\t\t\tEnter Rental ID: ";
+				cin >> r_id;
+				cin.ignore();
+				Rental r = getRental(r_id);
+				if (!r.chkPaid()) 
 				{
-					cout << "\t\t\t\tChange: " << (cp - cst) << endl;
-				}
-				cout << "\n======================================================================\n" << endl;
-			} 
-			else 
-			{
-				cout << "\t\t\t\tBill is already Paid" << endl;
-			}
-			waitForKeyPress();
-			clear();
-		} 
+					double cst = r.calculateBill();
+					clear();
+					cout << "============================= Bill ===================================\n" << endl;
+					r = getRental(r_id);
+					cout << r << endl;
+					cout << "======================================================================" << endl;
+					cout << "\t\t\tCost to Pay: " << cst << "\t\t" << endl;
+					cout << "\n====================================================================\n" << endl;
+					double cp;
+					do {
+						cout << "\t\t\t\tEnter Cost Paid: ";
+						cin >> cp;
+						cin.ignore();
+						if (cp < cst) {
+							cout << "\t\t\t\tInsufficient Amount" << endl;
+						}
+					} while (cp < cst);
 
-		else if (opt == 11) 
-		{
-			break;
-		} 
-	} while(opt != 11);
+					if (cp > cst) 
+					{
+						cout << "\t\t\t\tChange: " << (cp - cst) << endl;
+					}
+					cout << "\n======================================================================\n" << endl;
+				} 
+				else 
+				{
+					cout << "\t\t\t\tBill is already Paid" << endl;
+				}
+				waitForKeyPress();
+				clear();
+
+			case 11:
+				break;
+		}
+	}
 
 	clear();
 	cout << "==================================== Thank You ====================================\n" << endl;
 	waitForKeyPress();
 	return 0;
+	cout << "Press any key to Continue...";
+	cin.ignore();
+	cin.get();
 }
+
